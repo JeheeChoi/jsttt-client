06cb69b392d73e0fd44531cab440081e649027ac
jeheeinsd <writetojehee@gmail.com>
2020-10-16 07:05:11 -0700

    Added trigger('reset') for auth 10/16
    Added reset ui for sign up/sign in/change pw

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -6,42 +6,48 @@ const onSignUpSuccess = function (res) {
     <p>New user
     ${user.email} successfully created!</p>
   `)
+  $('#sign-up').trigger('reset')
 }
 const onSignUpError = function (error) {
   $('#sign-up-display').text('Error Signing Up Code: ' + error.statusText)
   console.log('error is:', error)
+  $('#sign-up').trigger('reset')
 }
 const onSignInSuccess = function (res) {
   const user = res.user
   store.user = res.user
-  $('#sign-in-display').html(`
+  $('#sign-in-out-display').html(`
     <p>Hello
     ${user.email}! </p>
   `)
+  $('#sign-in').trigger('reset')
 }
 const onSignInError = function (error) {
-  $('#sign-in-display').text('Error Signing In Code: ' + error.statusText)
+  $('#sign-in-out-display').text('Error Signing In Code: ' + error.statusText)
   console.log('error is:', error)
+  $('#sign-in').trigger('reset')
 }
 
 const onSignOutSuccess = function () {
-  $('#sign-out-display').html(`
+  $('#sign-in-out-display').html(`
     <p>User ${store.user.email} successfully signed out! </p>
   `)
 }
 
 const onSignOutError = function (error) {
-  $('#sign-out-display').text('Error Signing Out Code: ' + error.statusText)
+  $('#sign-in-out-display').text('Error Signing Out Code: ' + error.statusText)
   console.log('error is:', error)
 }
 
 const onChangePwSuccess = function () {
   $('#change-pw-display').text('Password successfully changed!')
+  $('#change-password').trigger('reset')
 }
 
 const onChangePwError = function (error) {
   $('#change-pw-display').text('Error Changing Password Code: ' + error.statusText)
   console.log('error is:', error)
+  $('#change-password').trigger('reset')
 }
 
 module.exports = {
